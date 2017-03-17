9903d2bd9d15129ead1b21d2ad1d01093adb2df4
Dennis <teyushen@gmail.com>
2017-03-18 01:22:50 +0800

    爬圖片

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # Crawler
+
+前幾天看到有人用 node.js 寫一個爬蟲續抓取 decard 的功能。因此就直接套用 [crawler4j](https://github.com/yasserg/crawler4j) 寫一個類似的功能。
+
+由於幾乎沒什麼需要特別處理的，所以就不多做介紹了。
+
+#### 簡單用法
+
+ImageCrawlController 中的 crawlDomains 是用來存放要爬的網頁。
+
+如果有一些爬曲網頁的邏輯需要調整，主要是修改 MyCrawler。
+
+
+
+### End
---
+++ src/main/java/service/ImageCrawlController.java
@@ -0,0 +1,47 @@
+package service;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import edu.uci.ics.crawler4j.crawler.CrawlConfig;
+import edu.uci.ics.crawler4j.crawler.CrawlController;
+import edu.uci.ics.crawler4j.fetcher.PageFetcher;
+import edu.uci.ics.crawler4j.robotstxt.RobotstxtConfig;
+import edu.uci.ics.crawler4j.robotstxt.RobotstxtServer;
+
+public class ImageCrawlController {
+	private static final Logger logger = LoggerFactory.getLogger(ImageCrawlController.class);
+
+    public static void main(String[] args) throws Exception {
+
+        String rootFolder = "~downloads/image";
+        int numberOfCrawlers = 2;
+        String storageFolder = "downloads/image/temp2";
+
+        CrawlConfig config = new CrawlConfig();
+
+        config.setCrawlStorageFolder(rootFolder);
+        config.setMaxDepthOfCrawling(1);
+        config.setPolitenessDelay(20);
+
+    /*
+     * Since images are binary content, we need to set this parameter to
+     * true to make sure they are included in the crawl.
+     */
+        config.setIncludeBinaryContentInCrawling(true);
+
+        String[] crawlDomains = {"https://www.dcard.tw/f/sex"};
+
+        PageFetcher pageFetcher = new PageFetcher(config);
+        RobotstxtConfig robotstxtConfig = new RobotstxtConfig();
+        RobotstxtServer robotstxtServer = new RobotstxtServer(robotstxtConfig, pageFetcher);
+        CrawlController controller = new CrawlController(config, pageFetcher, robotstxtServer);
+        for (String domain : crawlDomains) {
+            controller.addSeed(domain);
+        }
+
+        MyCrawler.configure(crawlDomains, storageFolder);
+
+        controller.start(MyCrawler.class, numberOfCrawlers);
+    }
+}
